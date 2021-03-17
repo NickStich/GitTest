b14c2e7f060784a46ae06f1d7fb0edea564764a5
Nick <nickstich@icloud.com>
2021-03-17 22:04:59 +0200

    first

---
+++ GitTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            TestMethod();
+        }
+
+        static void TestMethod()
+        {
+            Console.WriteLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
+                "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+        }
+    }
+}
